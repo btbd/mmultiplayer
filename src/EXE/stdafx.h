f627b4ac5a5d0e5b8ebf5e6737e73d5c00c6d3ff
btbd <b4davis88@gmail.com>
2017-11-22 19:25:35 -0500

    Minor changes

--- src/EXE/stdafx.h
+++ src/EXE/stdafx.h
@@ -57,7 +57,7 @@ typedef struct {
 #define SERVER_PORT 2783
 #define CLIENT_PORT 7319
 // #define IP "127.0.0.1"
-#define IP "169.46.24.47"
+#define IP "169.46.30.44"
 
 enum {
 	CHARACTER_FAITH = 0,
